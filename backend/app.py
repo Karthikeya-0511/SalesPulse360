a3e621cd40a3690b4f231f741394ef3b5ef92110
Karthikeya-0511 <sriramojukarthikeya27@gmail.com>
2026-07-07 16:19:58 +0530

    fixed cors

--- backend/app.py
+++ backend/app.py
@@ -28,7 +28,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:8080",
-        "http://127.0.0.1:8080"
+        "http://127.0.0.1:8080",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://salespulse360-frontend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
